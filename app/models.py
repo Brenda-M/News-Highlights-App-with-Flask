3d52e69b70be385bd2a431579f1ae9793ba23eaa
Brenda-M <brendanjuguna1@gmail.com>
2020-04-27 00:28:55 +0300

    define a news source class and define the paramenters needed in the news objects

---
+++ app/models.py
@@ -0,0 +1,13 @@
+class Sources:
+  '''
+  Defines sources objects
+
+  '''
+  def __init__(self, id, name, description, category, language, country, url):
+    self.id = id
+    self.name = name
+    self.description = description
+    self.category = category
+    self.country = country
+    self.language = language
+    self.url = url
--- config.py
+++ config.py
@@ -5,7 +5,8 @@ class Config:
   General configuration parent class
 
   '''
-  pass
+  NEWS_BASE_URL = 'https://newsapi.org/v2/{}?apiKey={}'
+  NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 class ProdConfig(Config):
   '''
