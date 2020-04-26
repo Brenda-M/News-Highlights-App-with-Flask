38451f7490b15bf9af03351aa916f06687904d81
Brenda-M <brendanjuguna1@gmail.com>
2020-04-26 23:51:03 +0300

    create function that takes in the configuration setting key as argument and create an instance of the application

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,13 @@
 from flask import Flask
 from config import config_options
+
+def create_app(config_name):
+  
+  app = Flask(__name__)
+
+  #Create the app configurations
+  app.config.from_object(config_options[config_name])
+
+  #Intialize flask extensions
+
+  return app
